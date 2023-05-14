ef47f82118876456fa3a8fb7d3b4e258080f9910
Sean Sponsler <seaneezer@yahoo.com>
2023-05-13 20:31:03 -0700

    three.js draft 1
    Original proposal:
    -Animation of man running, speeds up via different algorithms. I found this too boring although I may end up implementing the algorithm speed controls
    
    ThreeJS: PerspectiveCamera(), OrbitControls() to control viewport rotation and zoom as well as camera control
    Using car example from threejs examples, used new GLB file of a plane, tracked down objects to deploy materials which can be user configured. 
    Applied equirectangular background map of HDR sunset.
    Added turbulence noise via body rotation (could be improved)
    
    To add before wednesday: performance slider

---
+++ Final-Project/main.js
@@ -0,0 +1,30 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import * as THREE from 'three';
+import Stats from 'three/addons/libs/stats.module.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
